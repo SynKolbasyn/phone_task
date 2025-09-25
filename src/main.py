cfc7daa2480bbf88884f6191192af2f7bc042ccd
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-09-25 19:27:02 +0300

    feat: added `LICENSE` and `README.md`

--- src/main.py
+++ src/main.py
@@ -1,3 +1,23 @@
+"""
+Phone Call Service.
+
+Copyright (C) 2025  Andrew Kozmin <syn.kolbasyn.06@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -12,7 +32,6 @@ app = FastAPI(
     version="0.1.0",
 )
 
-# Настройка CORS (на всякий случай)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -21,7 +40,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Подключаем роутеры
 app.include_router(api_router)
 
 
