1829d7cea8b4fbd379cc76fb12385285d632d35e
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-09-25 18:24:40 +0300

    feat: calls dev

--- src/worker/celery_app.py
+++ src/worker/celery_app.py
@@ -5,7 +5,7 @@ from core.logging import setup_logging
 
 setup_logging()
 
-app = Celery(backend=Settings().redis_url, broker=Settings().redis_url, include="tasks")
+app = Celery(backend=Settings().redis_url, broker=Settings().redis_url, include="worker.tasks")
 
 app.conf.update(
     task_serializer="json",
